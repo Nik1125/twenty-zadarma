6e00cd57f0a68fd660d2e47cf4d88147e19e118d
Nik1125 <aleksyeyenko.nikita@gmail.com>
2026-05-05 17:26:12 +0200

    Initial commit from Create Twenty App

---
+++ src/constants/universal-identifiers.ts
@@ -0,0 +1,4 @@
+export const APP_DISPLAY_NAME = 'Zadarma';
+export const APP_DESCRIPTION = 'Zadarma telephony integration: incoming/outgoing calls and SMS, callLog/smsLog objects, settings UI';
+export const APPLICATION_UNIVERSAL_IDENTIFIER = '4b94c177-fa2a-449c-8b75-a8cc3bbc8285';
+export const DEFAULT_ROLE_UNIVERSAL_IDENTIFIER = '4f8dabaf-2a41-42d1-a12b-f1c8f08be1b4';
